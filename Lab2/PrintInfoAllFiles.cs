d4444e31d33bc93a19425af92712de8a5447ca11
Berco Andrei <andrei2003berco@gmail.com>
2023-10-19 13:38:58 +0300

    add class and methods for info allfiles command

---
+++ Lab2/PrintInfoAllFiles.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class PrintInfoAllFiles
+{
+	private string filePath= @"C:\\Users\Andrei\Desktop\TestOOP\";
+    
+	public void PrintInfo()
+	{
+        string[] files = Directory.GetFiles(filePath);
+        foreach (var item in files)
+        {
+            FileInfo fileInfo = new FileInfo(item);
+            Console.WriteLine("File Name: " + fileInfo.Name);
+            Console.WriteLine("Extension: " + fileInfo.Extension);
+            Console.WriteLine("Created: " + fileInfo.CreationTime);
+            Console.WriteLine("Last Updated: " + fileInfo.LastWriteTime);
+        }
+    }
+}
+	
--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//info allfiles
+PrintInfoAllFiles printAllFilesInfo =new PrintInfoAllFiles();
+printAllFilesInfo.PrintInfo();
