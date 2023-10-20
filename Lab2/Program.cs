ef357c9c41729c8bf48fa1a1e017b156d17fac71
Berco Andrei <andrei2003berco@gmail.com>
2023-10-20 09:21:05 +0300

    start program loop and implement classes added before

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,24 +1,67 @@
 ﻿using Lab2;
 
-//info allfiles
-PrintInfoAllFiles printAllFilesInfo = new PrintInfoAllFiles();
-printAllFilesInfo.PrintInfoFiles();
 
-//info image file.png
-PrintInfoImageFiles printInfoImageFiles = new PrintInfoImageFiles();
-printInfoImageFiles.PrintInfoSpecificFile("test.png");
-printInfoImageFiles.PrintImageSize("test.png");
 
-//info text file.txt
-PrintInfoTextFiles printInfoTextFiles = new PrintInfoTextFiles();
-printInfoTextFiles.LinesCount("BazeDeDate.txt");
-printInfoTextFiles.WordCount("BazeDeDate.txt");
-printInfoTextFiles.CharactersCount("BazeDeDate.txt");
-printInfoTextFiles.PrintFileInfo();
 
-//info programFiles
-PrintInfoProgramFiles printInfoProgramFiles = new PrintInfoProgramFiles();
-printInfoProgramFiles.CountLines("FileManager.cs");
-printInfoProgramFiles.CountClasses("FileManager.cs");
-printInfoProgramFiles.CountMethods("FileManager.cs");
-printInfoProgramFiles.PrintProgramFileInfo();
+
+
+
+
+
+string option = "";
+Console.WriteLine("Available options: ");
+Console.WriteLine("1. commit");
+Console.WriteLine("2. info <filename> (allfiles, image, text, program)");
+Console.WriteLine("3. status");
+while (option != "q")
+{
+    option = Console.ReadLine();
+    string[] splitOption = option.Split(' ');
+    switch (splitOption[0]) 
+    {
+        case "commit":
+            break;
+        case "info":
+            string[] getExtension = splitOption[1].Split('.');
+            switch (getExtension.Length) 
+            {
+                case 1:
+                    //info allfiles
+                    PrintInfoAllFiles printAllFilesInfo = new PrintInfoAllFiles();
+                    printAllFilesInfo.PrintInfoFiles();
+                    break;
+                case 2:
+                    switch (getExtension[1])
+                    {
+                        case "png":
+                            //info image file.png  +++
+                            PrintInfoImageFiles printInfoImageFiles = new PrintInfoImageFiles();
+                            printInfoImageFiles.PrintInfoSpecificFile(splitOption[1]);
+                            printInfoImageFiles.PrintImageSize(splitOption[1]);
+                        break;
+                        case "txt":
+                            //info text file.txt
+                            PrintInfoTextFiles printInfoTextFiles = new PrintInfoTextFiles();
+                            printInfoTextFiles.PrintInfoSpecificFile(splitOption[1]);
+                            printInfoTextFiles.LinesCount(splitOption[1]);
+                            printInfoTextFiles.WordCount(splitOption[1]);
+                            printInfoTextFiles.CharactersCount(splitOption[1]);
+                            printInfoTextFiles.PrintFileInfo();
+                            break;
+                        default:
+                            //info programFiles
+                            PrintInfoProgramFiles printInfoProgramFiles = new PrintInfoProgramFiles();
+                            printInfoProgramFiles.PrintInfoSpecificFile(splitOption[1]);
+                            printInfoProgramFiles.CountLines("FileManager.cs");
+                            printInfoProgramFiles.CountClasses("FileManager.cs");
+                            printInfoProgramFiles.CountMethods("FileManager.cs");
+                            printInfoProgramFiles.PrintProgramFileInfo();
+                            break;
+                    }
+                    break;
+
+
+            }
+            break;
+    }
+}
