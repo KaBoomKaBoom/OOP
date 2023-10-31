54691532cfe131cdd2453821c95c2d9e9e713e8a
Berco Andrei <andrei2003berco@gmail.com>
2023-10-31 16:56:33 +0200

    add class for console menu (needs future modification)

---
+++ Lab3/ConsoleMenu.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+public class ConsoleMenu
+{
+	public void PrintOperations() 
+	{
+		Console.WriteLine("1. aus - Array Up Stack");
+		Console.WriteLine("2. ads - Array Down Stack");
+		Console.WriteLine("3. lls - Linked List Stack");
+		Console.WriteLine("4. auq - Array Up Queue");
+		Console.WriteLine("5. adq - Array Down Queue");
+		Console.WriteLine("6. llq - Linked List Queue");
+		Console.WriteLine("7. q - Quit");
+		Console.Write(">>> ");
+	}
+
+	public int GetCapacity() 
+	{
+		Console.Write("Capacity: ");
+		return Convert.ToInt32(Console.ReadLine()); ;
+	}
+
+	public void ExecOperations() 
+	{
+		string option;
+		while (true) 
+		{
+			PrintOperations();
+			option = Console.ReadLine();
+
+			//implement a class: switch case for operations with stack/queue !!!!!!!!
+			switch (option) 
+			{
+				case "aus":
+					var capacity = GetCapacity();
+					var arrayUpStack = new ArrayUpStack<int>(capacity);
+
+					break;
+				case "ads":
+					capacity = GetCapacity();
+					var arrayDownStack = new ArrayDownStack<int>(capacity);
+
+					break;
+				case "lls":
+					capacity = GetCapacity();
+					var linkedListStack = new StackLinkList<int>();
+
+					break;
+				case "auq":
+					capacity = GetCapacity();
+					var arrayUpQueue = new ArrayUpQueue<int>(capacity);
+
+					break;
+				case "adq":
+					capacity = GetCapacity();
+					var arrayDownQueu = new ArrayDownQueue<int>(capacity);
+
+					break;
+				case "llq":
+					var linkedListQueue = new QueueLinkList<int>();
+
+					break;
+			}
+		}
+	}
+}
