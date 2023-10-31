35c15bca0200ad8bd2ceeb9f790f28b54e36422b
Berco Andrei <andrei2003berco@gmail.com>
2023-10-31 14:49:01 +0200

    add linked list  implementation for stack

---
+++ Lab3/StackLinkList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class StackLinkList<T> : IStack<T>
+{
+	private LinkedList<T> stack;
+	public StackLinkList()
+	{
+		stack = new LinkedList<T>();
+	}
+
+	public int Count => stack.Count;
+	public bool IsEmpty => Count == 0;
+
+	public void Push(T item) 
+	{
+		stack.AddLast(item);
+	}
+
+	public T Pop() 
+	{
+		if (!IsEmpty)
+		{
+			var item = stack.Last.Value;
+			stack.RemoveLast();
+			return item;
+		}
+		throw new InvalidOperationException("Stack is empty.");
+	}
+
+	public T Peek()
+	{
+		if (!IsEmpty)
+			return stack.Last.Value;
+		throw new InvalidOperationException("Stack is empty.");
+	}
+
+	public void Clear()
+	{
+		stack.Clear();
+	}
+}
