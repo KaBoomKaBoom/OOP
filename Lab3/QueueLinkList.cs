45e2f25301cf5a50ec625514c57b9a0c3b0f7d05
Berco Andrei <andrei2003berco@gmail.com>
2023-10-31 16:31:28 +0200

    add linked list implementation for queue

---
+++ Lab3/QueueLinkList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class QueueLinkList<T> : IQueue<T>
+{
+	private LinkedList<T> queue;
+	public QueueLinkList()
+	{
+		queue = new LinkedList<T>();
+	}
+
+	public int Count => queue.Count;
+	public bool IsEmpty => Count == 0;
+
+	public void Enqueue(T item)
+	{
+		queue.AddLast(item);
+	}
+
+	public T Dequeue()
+	{
+		if (!IsEmpty)
+		{
+			var item = queue.First.Value;
+			queue.RemoveFirst();
+			return item;
+		}
+		throw new InvalidOperationException("Queue is empty.");
+	}
+
+	public T Peek()
+	{
+		if (!IsEmpty)
+			return queue.First.Value;
+		throw new InvalidOperationException("Queue is empty.");
+	}
+
+	public void Clear()
+	{
+		queue.Clear();
+	}
+}
